d22c4f9855996b8e1bec0a1da7083859b0d7a6b1
henel321 <henrik@tillander.org>
2020-10-12 17:07:38 +0200

    Added brew packages

--- brew_install.sh
+++ brew_install.sh
@@ -2,7 +2,9 @@
 
 ## install a bunch of brew packages
 cd `dirname $0`
+brew install cask
 brew install `cat brew_packages.txt`
+brew cask install `cat brew_cask_packages.txt`
 
 ## create a ~/.fzf.zsh file
 $(brew --prefix)/opt/fzf/install --no-update-rc --no-bash --all
